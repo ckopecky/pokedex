a1521f8f3aff2799c9d7938e203d28f39b97bc7e
ckopecky <kopecky12112@gmail.com>
2019-10-19 20:10:19 -0700

    Add CDM to Card to get indiv pokemon info

--- pokedex/src/components/Card.js
+++ pokedex/src/components/Card.js
@@ -1,6 +1,18 @@
 import React from 'react';
 
-const Card = (props) => {
+    componentDidMount() {
+        axios.get(`${this.props.pokemon.url}`)
+            .then(response => {
+              
+                const { name, abilities, base_experience, id, height, sprites, types, weight } = response.data;
+                let uppercase = name[0].toUpperCase() + name.slice(1);
+                let newId = id.toString().length < 3 ? id.toString().padStart(3, '0') : id;
+                let fancyPic = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${newId}.png`;
+                console.log(fancyPic)
+                sprites.front_default = fancyPic;
+                this.setState({ name: uppercase, abilities, exp: base_experience, id, height, sprites, types, weight})
+            })
+    }
     return (
         <div>
             {props.name}
